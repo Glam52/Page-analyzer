f3c0d4d42c5c768d2f1dbc7961cd393f1d8344f2
Glam52 <gla_max@outlook.com>
2024-08-26 10:35:00 +0700

    final working with step 4

--- database.sql
+++ database.sql
@@ -1,7 +1,21 @@
-CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table urls
+(
+    id         serial
+        primary key,
+    name       varchar(255) not null,
+    created_at timestamp default CURRENT_TIMESTAMP
 );
 
-SELECT * FROM urls;
+alter table urls
+    owner to postgres;
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL,
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (url_id) REFERENCES urls (id)
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -50,11 +50,18 @@ def add_url():
 def list_urls():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM urls ORDER BY created_at DESC")
+    cur.execute("""
+        SELECT urls.id, urls.name, MAX(url_checks.created_at) as last_check
+        FROM urls
+        LEFT JOIN url_checks ON urls.id = url_checks.url_id
+        GROUP BY urls.id
+        ORDER BY urls.created_at DESC
+    """)
     urls = cur.fetchall()
     cur.close()
     conn.close()
     return render_template('list_urls.html', urls=urls)
+
 
 
 
@@ -64,13 +71,37 @@ def show_url(id):
     cur = conn.cursor()
     cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
     url = cur.fetchone()
+
+    # Получение всех проверок по url_id
+    cur.execute("SELECT id, created_at FROM url_checks WHERE url_id = %s ORDER BY created_at DESC", (id,))
+    checks = cur.fetchall()
     cur.close()
     conn.close()
 
     if url is None:
         abort(404)
 
-    return render_template('show_url.html', url=url)
+    return render_template('show_url.html', url=url, checks=checks)
+
+
+@app.route("/urls/<int:id>/checks", methods=["POST"])
+def create_check(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Создание новой проверки с url_id
+    cur.execute(
+        "INSERT INTO url_checks (url_id) VALUES (%s) RETURNING id, created_at;",
+        (id,)
+    )
+    conn.commit()
+    check_id, created_at = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    return redirect(f"/urls/{id}")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
