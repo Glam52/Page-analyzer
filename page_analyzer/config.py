c355c57844681fa012e943a029072a9c0067dfb4
Glam52 <gla_max@outlook.com>
2024-09-21 10:54:41 +0700

    work with database for deploing

--- page_analyzer/config.py
+++ page_analyzer/config.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
+# Загружаем переменные окружения из файла secret.env
+load_dotenv(dotenv_path='./secret.env')
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "blablabla")
-    DATABASE_URL = os.environ.get(
-        "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/hexlet"
-    )
+    SECRET_KEY = os.getenv("SECRET_KEY", "blablabla")  # Здесь можно оставить значение по умолчанию
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    if DATABASE_URL is None:
+        raise ValueError("DATABASE_URL environment variable is not set.")
