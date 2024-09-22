4e88e63d77d5f3ac5ad32ff9e659fc1f3f32ca3f
Glam52 <gla_max@outlook.com>
2024-09-22 12:02:47 +0700

    changing database

--- page_analyzer/config.py
+++ page_analyzer/config.py
@@ -9,7 +9,7 @@ class Config:
     # Здесь можно оставить значение по умолчанию
     SECRET_KEY = os.getenv("SECRET_KEY", "blablabla")
     DATABASE_URL = os.getenv("DATABASE_URL",
-                             "postgresql://postgres:postgres@localhost:5432/hexlet")
+                             "postgresql://hexlet_428q_user:URj6a9NJ9Q20qdpKbI0S5eKeddjsxQiq@dpg-crn3ss56l47c73a97rng-a.oregon-postgres.render.com/hexlet_428q")
 
     if DATABASE_URL is None:
         raise ValueError("DATABASE_URL environment variable is not set.")
