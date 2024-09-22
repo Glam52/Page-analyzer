af21fc6d0d8015caea4c9840bf51c65bd34afc2b
Glam52 <gla_max@outlook.com>
2024-09-22 13:46:44 +0700

    changing build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Установите переменную DATABASE_URL непосредственно в скрипте
-DATABASE_URL="postgresql://postgres:postgres@localhost:5432/hexlet"
+DATABASE_URL="postgresql://hexlet_428q_user:URj6a9NJ9Q20qdpKbI0S5eKeddjsxQiq@dpg-crn3ss56l47c73a97rng-a.oregon-postgres.render.com/hexlet_428q"
 
 # Выполните команды, используя эту переменную
 make install && psql -a -d "$DATABASE_URL" -f database.sql
